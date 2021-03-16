cdbcb59e110ced0aea2fc94f0e0551e949fd49c5
SmrutiSingh12 <66952472+SmrutiSingh12@users.noreply.github.com>
2021-03-16 18:42:09 +0530

    reverse

---
+++ reverseintandstring.py
@@ -0,0 +1,21 @@
+
+a=[]
+
+print("enter the input")
+a=list(map(int,input().strip().split()))
+print(a)
+
+print(a[::-1])
+
+s=input("enter the string:")
+s1=s[::-1]
+print(s1)
+
+'''
+enter the input
+1 2 3
+[1, 2, 3]
+[3, 2, 1]
+enter the string:abcd
+dcba
+'''
